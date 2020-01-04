97f1c8c7b1210c5556078f0d41df42f382a48efb
Kristov Waruwu <Kristoveykw@gmail.com>
2020-01-04 20:27:47 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,3 +1,30 @@
+<style>
+    nav a:hover{
+        color:#000;
+        bottom: 30%;
+     }
+    
+    nav a::before {
+    content: '';
+    display: block;
+    height: 4px;
+    background-color: #444;
+    bottom: 100%;
+    top: 90%;
+    width: 0;
+    position: absolute;
+    
+    
+
+    -webkit-transition: all ease-in-out 250ms;
+
+    transition: all ease-in-out 250ms;
+    }
+    nav a:hover::before {
+    width:4%;
+    }
+
+</style>
     <div class="navpintar container">
         <nav class="navbar navbar-expand-lg">
             <a class="navbar-brand" href="#">SAKAI</a>
